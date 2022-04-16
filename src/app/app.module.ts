fd1086c87635742c4ad03bac790898313766b75f
chepceline <chepceline25@gmail.com>
2022-04-16 03:00:48 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { NealComponentComponent } from './neal-component/neal-component.component';
+import { TheParentComponentComponent } from './Basics/Component-Data-Binding/child-parent/the-parent-component/the-parent-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    NealComponentComponent,
+    TheParentComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-component/my-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-cool-component',
+  templateUrl: './my-component.html',
+  // template: '<p>This is the coolest component</p>',
+  // styles: ['div{color:green}'],
+  styleUrls: [],
+})
+export class MyCoolComponent {}
