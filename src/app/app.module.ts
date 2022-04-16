9f2cf3560489e5e8b465d0f25fd361de4d034083
chepceline <chepceline25@gmail.com>
2022-04-16 03:11:18 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
 import { NealComponentComponent } from './neal-component/neal-component.component';
 import { TheParentComponentComponent } from './Basics/Component-Data-Binding/child-parent/the-parent-component/the-parent-component.component';
+import { ChildComponentComponent } from './Basics/Component-Data-Binding/parent-child/child-component/child-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyComponentComponent,
     NealComponentComponent,
-    TheParentComponentComponent
+    TheParentComponentComponent,
+    ChildComponentComponent
   ],
   imports: [
     BrowserModule,
